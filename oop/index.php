49868f49208f405a2231acf34898663148bf48b8
Kento Takano <kento2github@gmail.com>
2020-03-08 19:37:39 +0900

    oop learn

--- oop/index.php
+++ oop/index.php
@@ -1,29 +1,101 @@
 <?php
 
 class Video {
-  public $type;
-  public $duration;
-  public $published = false;
-  public $title;
+  private $type;
+  private $duration;
+  private $published = false;
+  private $title;
+  private $playStatus = false;
+
+  public function __construct($type = 'mp4', $duration = '08:30', $title = 'default')
+  {
+    $this->type = $type;
+    $this->duration = $duration;
+    $this->title = $title;
+  }
+
+  public function setPublished($state)
+  {
+    $this->published = $state;
+  }
+
+  public function getPublished()
+  {
+    return $this->published;
+  }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * @param string $duration
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
 
   public function play()
   {
-    return $this->published ? "this video is playing" : "this video is not yet available";
+      if($this->getPublished()) {
+        $this->playStatus = true;
+        return "this video is playing";
+      }
+      return "this video is not yet available";
+
   }
 
   public function pause()
   {
     return $this->published ? "the video is paused" : "";
   }
+
+  public function __destruct()
+  {
+    var_dump('Destructing ...', $this);
+  }
 }
 
-header('Content-Type:text/plain', true);
+//header('Content-Type:text/plain', true);
 
-$introduction = new Video();
-$introduction->published = true;
-$introduction->author = 'Terry O';
-echo $introduction->play() , PHP_EOL, $introduction->pause() , PHP_EOL , $introduction->author, PHP_EOL, PHP_EOL;
-
-$video2 = new Video();
-echo $video2->play() , PHP_EOL, $video2->pause() , PHP_EOL;
-var_dump($introduction);
+$introduction = new Video('mp4', '10.30', 'Introduction to OOP');
+$introduction->setPublished(true);
+echo $introduction->play();
