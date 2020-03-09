2b5802ee22bc4b10a21087db983069d4820d4b2e
Kento Takano <kento2github@gmail.com>
2020-03-10 08:58:11 +0900

    Added bug tracking app

---
+++ bug-tracking-app/index.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$application = new \App\Helpers\App();
+
+echo $application->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
+echo $application->getLogPath() . PHP_EOL;
+echo $application->getEnvironment() . PHP_EOL;
+echo $application->isDebugMode() . PHP_EOL;
+
+if($application->isRunningFromConsole()) {
+    echo 'from console';
+} else {
+    echo 'from browser';
+}
